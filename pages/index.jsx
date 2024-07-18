a134c7e16603e3ee22dd8dee474ecb2173781b6c
Leehansol <gksthf996@naver.com>
2024-07-18 13:07:25 +0900

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import ProductList from '@/components/ProductList';
+
+function ProductPage() {
+	return (
+		<div>
+			<h1>상품목록 페이지</h1>
+			<ProductList />
+		</div>
+	);
+}
+
+export default ProductPage;
